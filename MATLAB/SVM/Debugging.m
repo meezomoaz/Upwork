3d1b4bd670ef0a1a4858273ddf868fb5d9099696
meezomoaz <62601580+meezomoaz@users.noreply.github.com>
2021-11-28 18:05:11 +0500

    Add files via upload

---
+++ MATLAB/SVM/Debugging.m
@@ -0,0 +1,15 @@
+% Load the Spam Email dataset
+% You will have X, y in your environment
+load('spamTrain.mat');
+C = 0.1;
+model = svmTrain(X, y, C, @linearKernel);
+
+p = svmPredict(model, X);
+fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
+
+% Load the test dataset
+% You will have Xtest, ytest in your environment
+load('spamTest.mat');
+
+p = svmPredict(model, Xtest);
+fprintf('Test Accuracy: %f\n', mean(double(p == ytest)) * 100);
